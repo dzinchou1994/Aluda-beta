cd9b5ef4bcd1b288eb8f2516c4a2e1757f3b3ee8
Mr Jincho <jincho@Mrs-MacBook-Pro.local>
2025-08-20 17:40:05 +0200

    UX: change loading label to 'Aluda ფიქრობს...'

--- components/ChatComposer.tsx
+++ components/ChatComposer.tsx
@@ -595,7 +595,7 @@ export default function ChatComposer({ currentChatId, onChatCreated, session }: 
                     <div className="w-2 h-2 bg-gray-400 dark:bg-gray-500 rounded-full animate-bounce" style={{ animationDelay: '150ms' }}></div>
                     <div className="w-2 h-2 bg-gray-400 dark:bg-gray-500 rounded-full animate-bounce" style={{ animationDelay: '300ms' }}></div>
                   </div>
-                  <span className="text-sm text-gray-500 dark:text-gray-400 ml-2">წერს...</span>
+                  <span className="text-sm text-gray-500 dark:text-gray-400 ml-2">Aluda ფიქრობს...</span>
                 </div>
               </div>
             </div>
