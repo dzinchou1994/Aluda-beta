0a02f3015ee95ea6beacac86a16f6f92ec2dc2cb
Mr Jincho <jincho@Mrs-MacBook-Pro.local>
2025-09-19 14:26:57 +0200

    chore(seo): set canonical baseUrl to https://www.aluda.ge for sitemap and robots sitemap ref

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://aluda.ge'
+  const baseUrl = 'https://www.aluda.ge'
   
   return [
     {
